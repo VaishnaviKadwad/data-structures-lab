690ab75decb1c20e0ec4528550d860a7890e0799
Vaishnavi Kadwad <vaishnavikadwad1@gmail.com>
2026-09-24 18:38:18 +0530

    Add sparse matrix

--- postfixStack.c
+++ postfixStack.c
@@ -1,79 +1,97 @@
+/*
+ * Evaluation of a postfix expression.
+ *
+ * Read one postfix expression from standard input and print its value.
+ * The lab sheet in the Brief pane is the whole contract: what a token is,
+ * what each of the six operators means, and what your program must print.
+ *
+ * HOW YOU SOLVE IT IS YOURS. A stack you write yourself, a plain array you
+ * index by hand, a linked list, recursion -- nothing here reads your source.
+ * Every test runs your program and reads the answer out of whatever it
+ * printed. Only the answer has to be right.
+ */
+
 #include <stdio.h>
-#include <ctype.h>
-
+#include<ctype.h>
 #define MAX 100
-
 int stack[MAX];
-int top = -1;
-
+int top=-1;
 void push(int x)
 {
-    stack[++top] = x;
-}
+    stack[++top]=x;
 
-int pop()
-{
+}
+int pop(){
     return stack[top--];
 }
-
-int main()
+int main(void)
 {
     char postfix[MAX];
-    int i, a, b, result;
+    int i,a,b, result;
+    int j;
 
-    printf("Enter postfix expression: ");
-    fgets(postfix, MAX, stdin);
+    fgets(postfix,MAX,stdin);
+    i=0;
 
-    i = 0;
-
-    while (postfix[i] != '\0')
+    while(postfix[i]!='\0')
     {
-        if (postfix[i] == ' ' || postfix[i] == '\n')
+        if (postfix[i]==' '|| postfix[i]=='\n')
         {
             i++;
         }
-        else if (isdigit(postfix[i]))
+        else if(isdigit((unsigned char)postfix[i]) ||
+                (postfix[i]=='-' && isdigit((unsigned char)postfix[i+1])))
         {
-            int num = 0;
+            int num=0;
+            int sign=1;
 
-            while (isdigit(postfix[i]))
+            if(postfix[i]=='-')
             {
-                num = num * 10 + (postfix[i] - '0');
+                sign=-1;
                 i++;
             }
 
-            push(num);
+            while(isdigit((unsigned char)postfix[i]))
+            {
+                num=num*10+(postfix[i]-'0');
+                i++;
+            }
+
+            push(sign*num);
         }
         else
         {
-            b = pop();
-            a = pop();
+            b=pop();
+            a=pop();
 
-            switch (postfix[i])
-            {
+            switch(postfix[i]){
                 case '+':
-                    result = a + b;
+                    result=a+b;
                     break;
 
                 case '-':
-                    result = a - b;
+                    result=a-b;
                     break;
 
                 case '*':
-                    result = a * b;
+                    result=a*b;
                     break;
 
                 case '/':
-                    result = a / b;
+                    result=a/b;
                     break;
 
                 case '%':
-                    result = a % b;
+                    result=a%b;
                     break;
 
-                default:
-                    printf("Invalid operator: %c\n", postfix[i]);
-                    return 1;
+                case '^':
+                    result=1;
+                    for(j=0;j<b;j++)
+                    {
+                        result=result*a;
+                    }
+                    break;
             }
 
             push(result);
@@ -81,7 +99,19 @@ int main()
         }
     }
 
-    printf("Result = %d\n", pop());
+    printf("%d",pop());
+
+    /* TODO: read the expression from standard input. It is one line of
+       tokens separated by spaces, and the line is the whole expression. */
+
+    /* TODO: tell an operand from an operator. An operator is a token of
+       exactly one character, one of + - * / % ^. A token like -5 is an
+       operand: a negative number, not a minus sign. */
+
+    /* TODO: evaluate it. In "a b op" the left operand is the one that
+       appeared first, so 9 4 - is 9 - 4 and not 4 - 9. */
+
+    /* TODO: print the value. It has to be the last number you print. */
 
     return 0;
 }
